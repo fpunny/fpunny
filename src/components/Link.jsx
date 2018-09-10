f3a2576d64f0d33bd4f7de1a089961af21bc0924
Frederic Pun <frederic.pun@gmail.com>
2018-09-10 15:58:46 -0400

    Some routing fix

--- src/components/Link.jsx
+++ src/components/Link.jsx
@@ -5,7 +5,6 @@ import { bindActionCreators } from 'redux';
 import { TRANSITION_DELAY } from '..';
 import { toggleFade } from '../redux/actions/page';
 
-const root = window.location.origin;
 export class __Link extends PureComponent {
   render () {
     const { to, className, children } = this.props;
@@ -16,11 +15,10 @@ export class __Link extends PureComponent {
 
   click = (e) => {
     e.preventDefault();
-    const { history, toggleFade, isActive } = this.props;
+    const { history, toggleFade, isActive, to } = this.props;
     if (isActive === undefined || !isActive) {
-      const path = e.currentTarget.href.replace(root, "");
       toggleFade();
-      window.setTimeout(() => history.push(path), TRANSITION_DELAY);
+      window.setTimeout(() => history.push(to), TRANSITION_DELAY);
     }
   }
 }
